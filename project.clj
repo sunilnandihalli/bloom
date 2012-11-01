3459722de1b5a3d89c63c2fe9ad0eb98fac589a6
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2012-11-01 13:38:13 +0530

    ....

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
                  [com.novemberain/monger "1.3.1"]
                  [cheshire "4.0.3"]
                  [org.clojure/clojure "1.4.0"]]
+  :resource-paths  ["/usr/local/lib/libthrift-0.8.0.jar"
+                    "/usr/local/lib/slf4j-log4j12-1.5.8.jar" "/usr/local/lib/slf4j-api-1.5.8.jar"]
   :source-paths ["src/clj/"]
   :java-source-paths [ "src/java/"]
   :main bloomier.core)
