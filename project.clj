a54f1f93beff4a83b9e434393573f6ce2056660d
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2012-11-01 19:54:30 +0530

    ...

--- project.clj
+++ project.clj
@@ -2,10 +2,15 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/core.cache "0.6.2"]
                  [com.novemberain/monger "1.3.1"]
+                 [thrift "0.8.0"]
+                 [slf4j-api "1.5.8"]
+                 [slf4j-log4j12 "1.5.8"]
+                 [log4j "1.2.14"]
                  [cheshire "4.0.3"]
                  [org.clojure/clojure "1.4.0"]]
   :resource-paths  ["/usr/local/lib/libthrift-0.8.0.jar"
                     "/usr/local/lib/slf4j-log4j12-1.5.8.jar" "/usr/local/lib/slf4j-api-1.5.8.jar"]
+  :plugins [[lein-localrepo "0.4.1"]]
   :source-paths ["src/clj/"]
   :java-source-paths [ "src/java/"]
   :main bloomier.core)
--- src/clj/bloomier/core.clj
+++ src/clj/bloomier/core.clj
@@ -1,8 +1,22 @@
 (ns bloomier.core
- (:import [com.github.sunilnandihalli. UserHistory$Iface UserHistory$Processor UserHistory$Client]
-          [bloomier Query Result])
- (:gen-class))
+  (:require handler)
+  (:import [com.github.sunilnandihalli.bloomier BloomFilter$Processor BloomFilter$Client]
+           [org.apache.thrift TException]
+           [org.apache.thrift.protocol TBinaryProtocol TProtocol]
+           [org.apache.thrift.server TServer TSimpleServer TThreadPoolServer]
+           [org.apache.thrift.transport TServerSocket TServerTransport]
+           handler)
+  (:require [debug :as d])
+  (:use debug)
+  (:gen-class (:main true)))
 
 
 
 
+(defn -main []
+  (let [handler (bloomier.handler.)
+        processor (BloomFilter$Processor. handler)
+        server-transport (TServerSocket. 9090)
+        ;server (TSimpleServer. processor server-transport)
+        server (TThreadPoolServer. processor server-transport)]
+    (.serve server)))
