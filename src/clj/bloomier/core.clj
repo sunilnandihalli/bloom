ab43d25f85c6b66fdb9f653855cd48f3a7967960
Sunil S Nandihalli <sunil.nandihalli@gmail.com>
2012-11-03 21:28:04 +0530

    ....

--- src/clj/bloomier/core.clj
+++ src/clj/bloomier/core.clj
@@ -1,43 +1,49 @@
 (ns bloomier.core
-  (:require bloomier.handler)
-  (:import [com.github.sunilnandihalli.bloomier BloomFilter$Processor BloomFilter$Client]
+  (:import [com.github.sunilnandihalli.bloomier BloomFilter$Processor BloomFilter$Client BloomFilter$Iface]
            [org.apache.thrift TException]
            [org.apache.thrift.protocol TBinaryProtocol TProtocol]
-           [org.apache.thrift.server TServer TSimpleServer TThreadPoolServer]
-           [org.apache.thrift.transport TServerSocket TServerTransport]
-           [bloomier handler])
-  (:require [debug :as d])
+           [org.apache.thrift.server TServer TSimpleServer TThreadPoolServer TThreadPoolServer$Args]
+           [org.apache.thrift.transport TServerSocket TServerTransport])
+  (:require [debug :as d]
+            [clojure.pprint :as pp])
   (:use debug)
   (:gen-class (:main true)))
 
 (defn -main []
-  (let [req-handler (reify com.github.sunilnandihalli.bloomier.BloomFilter$Iface
+  (let [req-handler (reify BloomFilter$Iface
                       (add [this membership-bf-id new-elem]
                         (display-local-bindings))
-                      (count [this counting-bf-id elem])
-                      (contains [this membership-bf-id elem])
-                      (dec [this counting-bf-id elem])
-                      (inc [this counting-bf-id elem])
-                      (bulk_contains [this list-of-membership-bf-id list-of-elems])
-                      (aged_inc [this aging-bf-id elem])
-                      (aged_dec [this aging-bf-id elem])
-                      (aged_count [this aging-bf-id elem])
-                      (commonality_add [this commonality-bf-id elem])
-                      (direct_commonality_add [this commonality-bf-id elem])
-                      (is_common [this commonality-bf-id elem])
-                      (delete_bloom_filter [this bf-id])
-                      (is_bloom_filter_present [this bf-id]))
+                      (count [this counting-bf-id elem]
+                        (display-local-bindings) 0)
+                      (contains [this membership-bf-id elem]
+                        (display-local-bindings) false)
+                      (dec [this counting-bf-id elem]
+                        (display-local-bindings))
+                      (inc [this counting-bf-id elem]
+                        (display-local-bindings))
+                      (bulk_contains [this list-of-membership-bf-id list-of-elems]
+                        (display-local-bindings) {})
+                      (aged_inc [this aging-bf-id elem]
+                        (display-local-bindings))
+                      (aged_dec [this aging-bf-id elem]
+                        (display-local-bindings))
+                      (aged_count [this aging-bf-id elem]
+                        (display-local-bindings) 0)
+                      (commonality_add [this commonality-bf-id elem]
+                        (display-local-bindings))
+                      (direct_commonality_add [this commonality-bf-id elem]
+                        (display-local-bindings))
+                      (is_common [this commonality-bf-id elem]
+                        (display-local-bindings) false)
+                      (delete_bloom_filter [this bf-id]
+                        (display-local-bindings))
+                      (is_bloom_filter_present [this bf-id]
+                        (display-local-bindings) false))
         processor (BloomFilter$Processor. req-handler)
         server-transport (TServerSocket. 9090)
-        #_ server
-       
-       #_ (TSimpleServer. (doto (org.apache.thrift.server.TSimpleServer$Args. server-transport)
-                            (.processor processor)))
-        server (TThreadPoolServer. (doto (org.apache.thrift.server.TThreadPoolServer$Args. server-transport)
+        server (TThreadPoolServer. (doto (TThreadPoolServer$Args. server-transport)
                                      (.processor processor)))]
     (.serve server)))
-
-(let [])
 
 #_ (let [{:keys [a b c]} {:a 10 :b 20 :c 40}]
  (d/d [a b c ]))
